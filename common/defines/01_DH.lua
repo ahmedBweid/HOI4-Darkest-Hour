f7686d29101b1ea8c6ec7852921f542754c0c9ab
MrBlazzar <MrBlazzar@gmail.com>
2017-08-31 14:32:49 -0400

    Surrender Event
    Added the Bitter Peace Surrender Event for Soviets surrendering to the Germans.
    
    Also added the localization for it and added some new flags to other events to account for the new events.
    
    Removed the rank_defines.lua and instead of having two luas just added it to the o1_dh.lua as well as changed opinions from -100-100 to -200-200 max values.
    
    Minor fixes to other files included.

--- common/defines/01_DH.lua
+++ common/defines/01_DH.lua
@@ -10,3 +10,12 @@ NDefines.NMilitary.FIELD_MARSHALL_CAP = 60
 NDefines.NMilitary.MIN_DIVISION_DEPLOYMENT_TRAINING = 0.3	
 NDefines.NProduction.MAX_MIL_FACTORIES_PER_LINE = 25
 NDefines.NProduction.MAX_NAV_FACTORIES_PER_LINE = 20
+--Opinion change from -100-100 to -200-200
+NDefines.NDiplomacy.MAX_OPINION_VALUE = 200
+NDefines.NDiplomacy.MIN_OPINION_VALUE = -200
+--Unit rank changes
+NDefines.NMilitary.TRAINING_MAX_LEVEL = 10
+NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
+NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04,	0.06,	0.08,	0.1,	0.14,	0.18,	0.22,	0.26,	0.3,	0.39,	0.48,	0.57,	0.66,	0.75,	0.78,	0.81,	0.84,	0.87,	0.9}		-- Experience needed to progress to the next level
+NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
+NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
