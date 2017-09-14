4366d8faed8362c608c269ad7cea84c53e51ab51
AlgerianGeneral <algerianmajor31000@gmail.com>
2017-09-14 22:08:35 +0100

    Elections + US MBTs

--- common/defines/01_DH.lua
+++ common/defines/01_DH.lua
@@ -13,12 +13,25 @@ NDefines.NMilitary.MIN_DIVISION_DEPLOYMENT_TRAINING = 0.3
 --Opinion change from -100-100 to -200-200
 NDefines.NDiplomacy.MAX_OPINION_VALUE = 200
 NDefines.NDiplomacy.MIN_OPINION_VALUE = -200
-NDefines.NAI.DIPLOMACY_FACTION_WRONG_IDEOLOGY_PENALTY = 0 -- 60 default AI penalty for diplomatic faction acitons between nations of different ideologies
-NDefines.NAI.DIPLOMACY_FACTION_SAME_IDEOLOGY_MAJOR = 0 -- 10 default AI bonus acceptance when being asked about faction is a major of the same ideology
-NDefines.NAI.DIPLOMACY_FACTION_NEUTRALITY_PENALTY = 0	-- 50 default Neutral nations have a separate penalty, not wanting to get involved at all, rather than caring much about the difference in ideology
 --Unit rank changes
 NDefines.NMilitary.TRAINING_MAX_LEVEL = 10
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04,	0.06,	0.08,	0.1,	0.14,	0.18,	0.22,	0.26,	0.3,	0.39,	0.48,	0.57,	0.66,	0.75,	0.78,	0.81,	0.84,	0.87,	0.9}		-- Experience needed to progress to the next level
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
 NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
+--Graphical Changes
+NDefines_Graphics.NMapIcons.STATES_PRIORITY_VICTORY_POINTS = 4
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 10.0 -- thickness in pixels -- 5
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 1.0 -- The center gradient is linear 1/255 per pixel for this many pixels
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 25.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 45.0 --11
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 2.0 --2
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 15.0 --20
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 50.0 --20
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_STRATEGIC_REGIONS = 0.973
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_HUE_MODIFIER = 0.0 -- 0.0
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.7 -- 0.6
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8 -- 0.8
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED = 50
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_KEY_SCALE = 0.02
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 15.0
