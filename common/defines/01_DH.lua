af11a246038f2eb839b51e3a1cc82f68cc5306d7
MrBlazzar <MrBlazzar@gmail.com>
2018-01-05 13:18:12 -0500

    Changes:
    Edited the define to fix a small error in the faction joining.
    
    Did a few edits to the government opinions startup event. Should work fine now, does for me at least.

--- common/defines/01_DH.lua
+++ common/defines/01_DH.lua
@@ -13,20 +13,23 @@ NDefines.NMilitary.MIN_DIVISION_DEPLOYMENT_TRAINING = 0.3
 --Opinion change from -100-100 to -200-200
 NDefines.NDiplomacy.MAX_OPINION_VALUE = 200
 NDefines.NDiplomacy.MIN_OPINION_VALUE = -200
+
 --Unit rank changes
 NDefines.NMilitary.TRAINING_MAX_LEVEL = 10
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04,	0.06,	0.08,	0.1,	0.14,	0.18,	0.22,	0.26,	0.3,	0.39,	0.48,	0.57,	0.66,	0.75,	0.78,	0.81,	0.84,	0.87,	0.9}		-- Experience needed to progress to the next level
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
 NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
+
 -- AI Diplomacy related
-NDefines.NAI.DIPLOMACY_FACTION_WRONG_IDEOLOGY_PENALTY = 0 -- AI penalty for diplomatic faction acitons between nations of different ideologies
+NDefines.NAI.DIPLOMACY_FACTION_WRONG_IDEOLOGY_PENALTY = 0 -- AI penalty for diplomatic faction actions between nations of different ideologies
 NDefines.NAI.DIPLOMACY_FACTION_SAME_IDEOLOGY_MAJOR = 10 -- AI bonus acceptance when being asked about faction is a major of the same ideology(edited)
 NDefines.NAI.DIPLOMACY_FACTION_PATERNAL_AUTOCRAT_PENALTY = 50  -- Neutral nations have a separate penalty, not wanting to get involved at all, rather than caring much about the difference in ideology
-NDefines.NAI.DIPLOMACY_FACTION_WRONG_IDEOLOGY_PENALTY = 10 -- AI penalty for diplomatic faction acitons between nations of different ideologies
+
 -- Naval Invasion Stuff
 NDefines.NNavy.NAVAL_SUPREMACY_INTEL_LOW = 0.3						-- we need more intel than this to get any supremacy
 NDefines.NNavy.NAVAL_SUPREMACY_CAN_INVADE = 0.8								-- required naval supremacy to perform invasions on an area. (Was 0.5)
+
 -- Naval Combat Stuff
 NDefines.NNavy.COMBAT_LOW_ORG_HIT_CHANCE_PENALTY = -0.6 -- -0.35 -- -0.8, -- % of penalty applied to hit chance when ORG is very low.
 NDefines.NNavy.COMBAT_DAMAGE_RANDOMNESS = 0.6 --0.3, -- random factor in damage. So if max damage is fe. 10, and randomness is 30%, then damage will be between 7-10.
@@ -43,7 +46,6 @@ NDefines.NNavy.REPAIR_AND_RETURN_PRIO_MEDIUM = 0.50 --0.5, -- % of total Strengt
 NDefines.NNavy.REPAIR_AND_RETURN_PRIO_HIGH = 0.65 --0.9, -- % of total Strength. When below, navy will go to home base to repair.
 NDefines.NNavy.COMBAT_DAMAGE_TO_STR_FACTOR = 1.0 --1.6, -- casting damage value to ship strength multiplier. Use it to balance the game difficulty.
 NDefines.NNavy.COMBAT_DAMAGE_TO_ORG_FACTOR = 1.9 --1.9, -- casting damage value to ship organisation multiplier. Use it to balance the game difficulty.
-
 
 --Graphical Changes
 NDefines_Graphics.NMapIcons.STATES_PRIORITY_VICTORY_POINTS = 4
